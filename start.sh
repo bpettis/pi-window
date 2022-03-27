f8050d44e7433fa4065b713bca9d1eda61bd6311
Ben Pettis <bpettis@wisc.edu>
2022-03-26 22:48:30 -0500

    use stretched aspect ratio

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ video_count=1
 
 echo "Attempting to launch VLC"
 # Pick a random line from the playlist and send that to VLC
-vlc -LZf $(shuf -n $video_count $playlist_name)
+cvlc -f --aspect-ratio=9:16 $(shuf -n $video_count $playlist_name)
 
 # --aspect-ratio=9:16
 
