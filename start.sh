6ffdd8ba8c6a5635ae63a2fd2b75b85971fde33a
Ben Pettis <bpettis@wisc.edu>
2022-03-26 22:37:37 -0500

    doing some adjustments locally like a scrub

--- start.sh
+++ start.sh
@@ -2,10 +2,16 @@
 
 echo "Starting fake window..."
 
-playlist_name="playlist.txt"
+echo "Setting DISPLAY variable..."
+export DISPLAY=':0'
+
+playlist_name="/home/pi/pi-window/playlist.txt"
 video_count=1
 
+echo "Attempting to launch VLC"
 # Pick a random line from the playlist and send that to VLC
 vlc -LZf $(shuf -n $video_count $playlist_name)
+
+# --aspect-ratio=9:16
 
 echo "Goodbye!"
